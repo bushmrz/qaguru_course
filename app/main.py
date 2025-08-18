e6d9267d4ea2deb260836912105ac949e2d18066
bushmrz <buuushmrz@mail.ru>
2025-08-18 22:23:51 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ dotenv.load_dotenv()
 import uvicorn
 from fastapi import FastAPI
 
-from routers import status, users
 from app.database.engine import create_db_and_tables
+from app.routers import status, users
 
 
 app = FastAPI()
